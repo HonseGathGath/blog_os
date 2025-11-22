140fcb565e2d1e7a095efd165d3356d4eeaacece
HonseGathGath <lassouedghaith15@gmail.com>
2025-11-22 21:52:54 +0000

    user space seperation and syscall interrupt '0x80'

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,13 +1,14 @@
-
-
-use conquer_once::spin::OnceCell;
-use crossbeam_queue::ArrayQueue;
-use core::{pin::Pin, task::{ Poll, Context}};
-use futures_util::{stream::Stream, task::AtomicWaker};
-use futures_util::stream::StreamExt;
-use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use crate::print;
 use crate::println;
+use conquer_once::spin::OnceCell;
+use core::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+use crossbeam_queue::ArrayQueue;
+use futures_util::stream::StreamExt;
+use futures_util::{stream::Stream, task::AtomicWaker};
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 static WAKER: AtomicWaker = AtomicWaker::new();
@@ -30,7 +31,8 @@ pub struct ScancodeStream {
 
 impl ScancodeStream {
     pub fn new() -> Self {
-        SCANCODE_QUEUE.try_init_once(|| ArrayQueue::new(100))
+        SCANCODE_QUEUE
+            .try_init_once(|| ArrayQueue::new(100))
             .expect("ScancodeStream::new should only be called once");
         ScancodeStream { _private: () }
     }
@@ -56,8 +58,7 @@ impl Stream for ScancodeStream {
 
 pub async fn print_keypresses() {
     let mut scancodes = ScancodeStream::new();
-    let mut keyboard  = Keyboard::new(ScancodeSet1::new(),
-                                     layouts::Us104Key, HandleControl::Ignore);
+    let mut keyboard = Keyboard::new(ScancodeSet1::new(), layouts::Azerty, HandleControl::Ignore);
 
     while let Some(scancode) = scancodes.next().await {
         if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
@@ -70,7 +71,3 @@ pub async fn print_keypresses() {
         }
     }
 }
-
-
-
-
